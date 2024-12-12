6c7f48eb514cfcc8f43d8f89ed10808d087b2616
Shaun <scpour@gmail.com>
2024-12-12 14:32:04 -0500

    Initial Commit

---
+++ src/Main.java
@@ -0,0 +1,46 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Slider;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main extends Application {
+
+    TextField bill;
+    Slider tip;
+    TextField party;
+    TextField total;
+    Button button;
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("Tip Calculator");
+
+        bill = new TextField();
+        tip = new Slider();
+        party = new TextField();
+        total = new TextField();
+        button = new Button();
+
+
+        StackPane layout = new StackPane();
+
+        layout.getChildren().add(bill);
+        layout.getChildren().add(tip);
+        layout.getChildren().add(party);
+        layout.getChildren().add(total);
+        layout.getChildren().add(button);
+
+        Scene scene = new Scene(layout, 300, 250);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+}
